e7755bcfafaa05dc703d3a115bc222e4380bd0ae
thomas-beauvais charles <thomasbeauvais.charles@gmail.com>
2016-07-25 14:07:40 +0200

    Add restclient to connect on Janus api and zipTopology to build a zip of the topology whith yml (describ the topo and csar for components)

---
+++ alien4cloud-janus-plugin/src/main/java/alien4cloud/plugin/Janus/rest/RestClient.java
@@ -0,0 +1,59 @@
+package alien4cloud.plugin.Janus.rest;
+
+import org.apache.commons.compress.utils.IOUtils;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.ClientHttpRequest;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.HttpMessageConverterExtractor;
+import org.springframework.web.client.RequestCallback;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.MediaType;
+
+import java.io.*;
+
+/**
+ * Created by a628490 on 22/07/2016.
+ */
+public class RestClient {
+
+    public void postTopologyToJanusTEST1() throws FileNotFoundException {
+        final InputStream fis = new FileInputStream(new File("")); // or whatever
+        final RequestCallback requestCallback = new RequestCallback() {
+            @Override
+            public void doWithRequest(final ClientHttpRequest request) throws IOException {
+                request.getHeaders().add("Content-type", "application/zip");
+                IOUtils.copy(fis, request.getBody());
+            }
+        };
+        final RestTemplate restTemplate = new RestTemplate();
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setBufferRequestBody(false);
+        restTemplate.setRequestFactory(requestFactory);
+        final HttpMessageConverterExtractor<String> responseExtractor =
+                new HttpMessageConverterExtractor<String>(String.class, restTemplate.getMessageConverters());
+        restTemplate.execute("http://localhost:8800/deployments", HttpMethod.POST, requestCallback, responseExtractor);
+    }
+
+    public void postTopologyToJanus() {
+        // Set the Content-Type header
+        HttpHeaders requestHeaders = new HttpHeaders();
+        requestHeaders.setContentType(new MediaType("application","zip"));
+        HttpEntity<String> requestEntity = new HttpEntity<>("topology.zip", requestHeaders);
+
+// Create a new RestTemplate instance
+        RestTemplate restTemplate = new RestTemplate();
+
+// Add the Jackson and String message converters
+        restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+        restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
+
+// Make the HTTP POST request, marshaling the request to JSON, and the response to a String
+        ResponseEntity<InputStream> responseEntity = restTemplate.exchange("http://localhost:8800/deployments", HttpMethod.POST, requestEntity, InputStream.class);
+        InputStream result = responseEntity.getBody();
+    }
+}
---
+++ alien4cloud-janus-plugin/src/main/java/alien4cloud/plugin/Janus/utils/ZipTopology.java
@@ -0,0 +1,99 @@
+package alien4cloud.plugin.Janus.utils;
+
+/**
+ * Created by a628490 on 22/07/2016.
+ */
+
+import alien4cloud.paas.model.PaaSNodeTemplate;
+import alien4cloud.paas.model.PaaSTopologyDeploymentContext;
+
+import java.io.*;
+import java.net.URI;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.zip.*;
+
+import static com.google.common.io.Files.copy;
+
+public class ZipTopology {
+
+    static final String CSAR = "csar/";
+
+    /**
+     *
+     * @param deploymentContext
+     * @return
+     */
+    public List<File> createListTopology(PaaSTopologyDeploymentContext deploymentContext) {
+        List<File> files = new LinkedList();
+        for (PaaSNodeTemplate node : deploymentContext.getPaaSTopology().getComputes()) {
+            String parentPathNode = node.getCsarPath().getParent().toString();
+            File fileN = new File(parentPathNode);
+            files.add(fileN);
+            List<PaaSNodeTemplate> children = node.getChildren();
+            for (PaaSNodeTemplate child : children) {
+                String parentPathChild = child.getCsarPath().getParent().toString();
+//                int index=parentPath.lastIndexOf('/');
+//                System.out.println(parentPath.substring(0,index));
+                File fileC = new File(parentPathChild);
+                files.add(fileC);
+            }
+        }
+        return files;
+    }
+
+    /**
+     *
+     * @param zipfile
+     * @param deploymentContext
+     * @throws IOException
+     */
+    public void buildZip(File zipfile, PaaSTopologyDeploymentContext deploymentContext) throws IOException {
+
+        List<File> folders = createListTopology(deploymentContext);
+        OutputStream out = new FileOutputStream(zipfile);
+        Closeable res;
+
+        ZipOutputStream zout = new ZipOutputStream(out);
+        //set the compression method and level
+//        zout.setMethod(ZipOutputStream.DEFLATED);
+//        zout.setLevel(9);
+        res = zout;
+        zout.putNextEntry(new ZipEntry(CSAR));
+        for (File directory : folders) {
+
+            //Get info name path for component
+            String[] dirFolders = directory.toString().split("/");
+            String componentName = dirFolders[dirFolders.length-2] + "/";
+            String componentVersion = dirFolders[dirFolders.length-1] + "/";
+
+            //create structure of our component folder
+            zout.putNextEntry(new ZipEntry(CSAR + componentName));
+            zout.putNextEntry(new ZipEntry(CSAR + componentName + componentVersion));
+
+            String struct = CSAR + componentName + componentVersion;
+
+            URI base = directory.toURI();
+            Deque<File> queue = new LinkedList<>();
+            queue.push(directory);
+            while (!queue.isEmpty()) {
+                directory = queue.pop();
+                for (File kid : directory.listFiles()) {
+                    String name = base.relativize(kid.toURI()).getPath();
+                    if (kid.isDirectory()) {
+                        queue.push(kid);
+                        name = name.endsWith("/") ? struct + name : struct + name + "/";
+                        zout.putNextEntry(new ZipEntry(name));
+                    } else {
+                        zout.putNextEntry(new ZipEntry(struct + name));
+                        copy(kid, zout);
+                    }
+                }
+            }
+        }
+        zout.putNextEntry(new ZipEntry("topology.yml"));
+        zout.closeEntry();
+        res.close();
+    }
+}
