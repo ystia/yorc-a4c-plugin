2425ad847d79c8c3ac2b04652c7ce19be6259214
LE <victor.le@atos.net>
2016-07-27 10:38:51 +0200

    Http request to Janus working

--- alien4cloud-janus-plugin/src/main/java/alien4cloud/plugin/Janus/rest/RestClient.java
+++ alien4cloud-janus-plugin/src/main/java/alien4cloud/plugin/Janus/rest/RestClient.java
@@ -7,24 +7,15 @@
 package alien4cloud.plugin.Janus.rest;
 
 import org.apache.commons.compress.utils.IOUtils;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.ClientHttpRequest;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
-import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.HttpMessageConverterExtractor;
 import org.springframework.web.client.RequestCallback;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.http.MediaType;
 
 import java.io.*;
 
-/**
- * Created by a628490 on 22/07/2016.
- */
 public class RestClient {
 
     public void postTopologyToJanusTEST1() throws FileNotFoundException {
@@ -46,20 +37,29 @@ public class RestClient {
     }
 
     public void postTopologyToJanus() {
-        // Set the Content-Type header
-        HttpHeaders requestHeaders = new HttpHeaders();
-        requestHeaders.setContentType(new MediaType("application","zip"));
-        HttpEntity<String> requestEntity = new HttpEntity<>("topology.zip", requestHeaders);
+        System.out.println(executeCommand("./curlUploadZipToJanus.sh"));
+    }
 
-// Create a new RestTemplate instance
-        RestTemplate restTemplate = new RestTemplate();
+    private static String executeCommand(String command) {
+        StringBuffer output = new StringBuffer();
 
-// Add the Jackson and String message converters
-        restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
-        restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
+        Process p;
+        try {
+            p = Runtime.getRuntime().exec(command);
 
-// Make the HTTP POST request, marshaling the request to JSON, and the response to a String
-        ResponseEntity<InputStream> responseEntity = restTemplate.exchange("http://10.0.0.4:8800/deployments", HttpMethod.POST, requestEntity, InputStream.class);
-        InputStream result = responseEntity.getBody();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            //p.waitFor();
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                System.out.println("line="+line);
+                output.append(line + "\n");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return output.toString();
+
     }
 }
