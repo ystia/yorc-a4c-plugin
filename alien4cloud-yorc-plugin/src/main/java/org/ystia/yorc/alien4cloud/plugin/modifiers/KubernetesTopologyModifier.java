bde4bf812d860e1749e63c604a59f1969504f551
Adriana Danes <adriana.danes@atos.net>
2018-03-15 17:43:53 +0100

    BRBDCF-755 clean-up code

--- alien4cloud-yorc-plugin/src/main/java/org/ystia/yorc/alien4cloud/plugin/modifiers/KubernetesTopologyModifier.java
+++ alien4cloud-yorc-plugin/src/main/java/org/ystia/yorc/alien4cloud/plugin/modifiers/KubernetesTopologyModifier.java
@@ -11,7 +11,6 @@ import org.alien4cloud.tosca.utils.TopologyNavigationUtil;
 
 import org.alien4cloud.tosca.model.templates.Topology;
 import org.alien4cloud.tosca.model.Csar;
-import org.alien4cloud.tosca.model.CSARDependency;
 import org.alien4cloud.tosca.model.templates.NodeTemplate;
 
 
@@ -37,19 +36,19 @@ public class KubernetesTopologyModifier extends TopologyModifierSupport {
     public static final String YORC_KUBERNETES_MODIFIER_TAG = "yorc-kubernetes-modifier";
 
     /**
-     * The below constants' values come from kubernetes resource types definitions:
-     * - template_name
-     * - template_version
+     * The below constant's values come from Yorc kubernetes resource types definitions:
+     * template_version: ${yorc.types.version}
      */
     protected static final String YORC_KUBERNETES_TYPES_ARCHIVE_NAME = "yorc-kubernetes-types";
     protected static final String YORC_KUBERNETES_TYPES_ARCHIVE_VERSION = "1.0.0-SNAPSHOT";
 
-    // A4C K8S resource types defined in org.alien4cloud.plugin.kubernetes.modifier
-    public static final String K8S_TYPES_DEPLOYMENT_RESOURCE = "org.alien4cloud.kubernetes.api.types.DeploymentResource";
-    public static final String K8S_TYPES_SERVICE_RESOURCE = "org.alien4cloud.kubernetes.api.types.ServiceResource";
     // Yorc K8S resource types
     protected static final String YORC_KUBERNETES_TYPES_DEPLOYMENT_RESOURCE = "yorc.nodes.kubernetes.api.types.DeploymentResource";
     protected static final String YORC_KUBERNETES_TYPES_SERVICE_RESOURCE = "yorc.nodes.kubernetes.api.types.ServiceResource";
+
+    // A4C K8S resource types defined in org.alien4cloud.plugin.kubernetes.modifier
+    public static final String K8S_TYPES_DEPLOYMENT_RESOURCE = "org.alien4cloud.kubernetes.api.types.DeploymentResource";
+    public static final String K8S_TYPES_SERVICE_RESOURCE = "org.alien4cloud.kubernetes.api.types.ServiceResource";
 
 
     @Resource(name="kubernetes-final-modifier")
@@ -77,40 +76,28 @@ public class KubernetesTopologyModifier extends TopologyModifierSupport {
     }
 
     private void doProcess(Topology topology, FlowExecutionContext context) {
-        log.info("~~~~~~~~~ Yorc Plugin : Processing topology " + topology.getId());
+        log.info("Yorc K8S Plugin : processing topology " + topology.getId());
 
         Csar csar = new Csar(topology.getArchiveName(), topology.getArchiveVersion());
 
-        // Import yorc-kubernetes-types
-        Csar yorcKubernetesTypesCsar = csarRepoService.getArchive(YORC_KUBERNETES_TYPES_ARCHIVE_NAME, YORC_KUBERNETES_TYPES_ARCHIVE_VERSION);
-        if (yorcKubernetesTypesCsar == null) {
-            // could not find the CSAR ... ?
-            log.info("~~~~~~~~~ Yorc Plugin : could not find " + YORC_KUBERNETES_TYPES_ARCHIVE_NAME + ":" + YORC_KUBERNETES_TYPES_ARCHIVE_VERSION);
-            return;
-        }
-
-        // not necessary since the replaceNode will ensure dependencies will be correct
-        //
-        // CSARDependency yorcKubernetesTypesDependency = new CSARDependency();
-        // yorcKubernetesTypesDependency.setName(YORC_KUBERNETES_TYPES_ARCHIVE_NAME);
-        // yorcKubernetesTypesDependency.setVersion(YORC_KUBERNETES_TYPES_ARCHIVE_VERSION);
-        // yorcKubernetesTypesDependency.setHash(yorcKubernetesTypesCsar.getDefinitionHash());
-        //
-        // topology.getDependencies().add(yorcKubernetesTypesDependency);
-        log.info("~~~~~~~~~ Yorc Plugin : Yorc kubernetes types archive added as dependency to the topology");
-
         // Transform alien kubernetes resource types to yorc kubernetes resource types
+        // Insure that Yorc kubernetes types archive added as dependency to the topology
         //
         // Treat deployment resource types
         transformKubernetesResourceTypes(topology,  csar, "deployment", YORC_KUBERNETES_TYPES_ARCHIVE_VERSION);
         // Treat service resource types
         transformKubernetesResourceTypes(topology,  csar, "service", YORC_KUBERNETES_TYPES_ARCHIVE_VERSION);
 
-
-        log.info("~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ");
-
     }
 
+    /**
+     * Replace alien kubernetes resource nodes by Yorc kubernetes resource nodes
+     * The replaceNode ensures that dependencies will be correct.
+     * @param topology topology to transform
+     * @param csar csar to transform
+     * @param resourceType the desired Yorc type
+     * @param resourceArchiveVersion the desired Yorc version
+     */
     private void transformKubernetesResourceTypes(Topology topology, Csar csar, String resourceType, String resourceArchiveVersion) {
         String sourceResourceType = null;
         String targetResourceType = null;
@@ -125,7 +112,7 @@ public class KubernetesTopologyModifier extends TopologyModifierSupport {
                 targetResourceType = YORC_KUBERNETES_TYPES_DEPLOYMENT_RESOURCE;
                 break;
             default:
-                log.info("~~~~~~~~~ Yorc Plugin : currently supported K8S resources are " + "service" + " and " + "deployment");
+                log.info("Yorc K8S Plugin : currently supported K8S resources are " + "service" + " and " + "deployment");
                 break;
         }
 
@@ -141,7 +128,7 @@ public class KubernetesTopologyModifier extends TopologyModifierSupport {
 
             NodeTemplate yorcServiceNodeTemplate = replaceNode(csar, topology, serviceNodeTemplate, effectiveTargetResourceType, resourceArchiveVersion);
 
-            log.info("~~~~~~~~~ Yorc Plugin : >>> service resource node " + yorcServiceNodeTemplate.getName() + " now has type " + yorcServiceNodeTemplate.getType());
+            log.info("Yorc K8S Plugin : k8s service resource node " + yorcServiceNodeTemplate.getName() + " now has type " + yorcServiceNodeTemplate.getType());
         });
     }
 
