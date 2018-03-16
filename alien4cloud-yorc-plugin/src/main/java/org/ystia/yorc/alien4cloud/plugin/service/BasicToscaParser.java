fffb4e600c20389b40ec369b02a51cdd7e3d5503
Albertin Loic <loic.albertin@atos.net>
2018-03-16 12:06:41 +0100

    Replace R_TARGET keyword in get_attributes by TARGET as in the specification

--- alien4cloud-yorc-plugin/src/main/java/org/ystia/yorc/alien4cloud/plugin/service/BasicToscaParser.java
+++ alien4cloud-yorc-plugin/src/main/java/org/ystia/yorc/alien4cloud/plugin/service/BasicToscaParser.java
@@ -2,7 +2,13 @@ package org.ystia.yorc.alien4cloud.plugin.service;
 
 import alien4cloud.tosca.model.ArchiveRoot;
 import alien4cloud.tosca.parser.ToscaParser;
+import org.alien4cloud.tosca.model.definitions.AbstractPropertyValue;
+import org.alien4cloud.tosca.model.definitions.ConcatPropertyValue;
+import org.alien4cloud.tosca.model.definitions.FunctionPropertyValue;
+import org.alien4cloud.tosca.model.types.AbstractInstantiableToscaType;
 import org.springframework.stereotype.Component;
+
+import static alien4cloud.utils.AlienUtils.safe;
 
 /**
  * A {@code BasicToscaParser} extends {@link ToscaParser} to remove all postParsing operations.
@@ -13,6 +19,36 @@ import org.springframework.stereotype.Component;
 public class BasicToscaParser extends ToscaParser {
     @Override
     protected void postParsing(ArchiveRoot result) {
-        // do not post process parsing
+        // Change R_TARGET to TARGET keyword
+        safe(result.getRelationshipTypes()).forEach(this::postProcessInstantiableType);
+        safe(result.getNodeTypes()).forEach(this::postProcessInstantiableType);
+    }
+
+    private void postProcessInstantiableType(String typeName, AbstractInstantiableToscaType type) {
+        safe(type.getAttributes()).forEach((an, a) -> {
+            if (!a.isDefinition() && a instanceof AbstractPropertyValue) {
+                postProcessPropVal((AbstractPropertyValue) a);
+            }
+        });
+
+        safe(type.getInterfaces())
+                .forEach((in, i) -> safe(i.getOperations()).forEach((on, o) -> safe(o.getInputParameters()).forEach((ipn, ip) -> {
+                    if (!ip.isDefinition() && ip instanceof AbstractPropertyValue) {
+                        postProcessPropVal((AbstractPropertyValue) ip);
+                    }
+                })));
+
+    }
+
+    private void postProcessPropVal(AbstractPropertyValue value) {
+        if (value instanceof ConcatPropertyValue) {
+            ConcatPropertyValue concatPropertyValue = (ConcatPropertyValue) value;
+            safe(concatPropertyValue.getParameters()).forEach(this::postProcessPropVal);
+        } else if (value instanceof FunctionPropertyValue) {
+            FunctionPropertyValue functionPropertyValue = (FunctionPropertyValue) value;
+            if ("R_TARGET".equals(functionPropertyValue.getParameters().get(0))) {
+                functionPropertyValue.getParameters().set(0, "TARGET");
+            }
+        }
     }
 }
