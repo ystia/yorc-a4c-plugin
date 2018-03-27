14b7c4ccdd61b293c34d94bd2b88184eecc528a2
Laurent Ganne <ganne.laurent@gmail.com>
2018-03-27 16:49:32 +0000

    Using TopologyModifierSupport.replaceNode to modify topology

--- alien4cloud-yorc-plugin/src/main/java/org/ystia/yorc/alien4cloud/plugin/modifiers/FipTopologyModifier.java
+++ alien4cloud-yorc-plugin/src/main/java/org/ystia/yorc/alien4cloud/plugin/modifiers/FipTopologyModifier.java
@@ -17,14 +17,15 @@ package org.ystia.yorc.alien4cloud.plugin.modifiers;
 
 import alien4cloud.paas.wf.validation.WorkflowValidator;
 import alien4cloud.tosca.context.ToscaContextual;
-import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
+import org.alien4cloud.tosca.catalog.index.IToscaTypeSearchService;
 import org.alien4cloud.alm.deployment.configuration.flow.FlowExecutionContext;
 import org.alien4cloud.alm.deployment.configuration.flow.TopologyModifierSupport;
 import org.alien4cloud.tosca.model.Csar;
-import org.alien4cloud.tosca.model.definitions.AbstractPropertyValue;
 import org.alien4cloud.tosca.model.templates.Capability;
 import org.alien4cloud.tosca.model.templates.NodeTemplate;
 import org.alien4cloud.tosca.model.templates.Topology;
+import org.alien4cloud.tosca.model.types.NodeType;
 import org.alien4cloud.tosca.utils.TopologyNavigationUtil;
 import org.springframework.stereotype.Component;
 
@@ -32,23 +33,25 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
+import javax.inject.Inject;
 
 /**
- * Transform a matched K8S topology containing <code>Container</code>s, <code>Deployment</code>s, <code>Service</code>s
- * and replace them with <code>DeploymentResource</code>s and <code>ServiceResource</code>s.
- * <p>
- * TODO: add logs using FlowExecutionContext
+ * Modifies an OpenStack topology, replacing each Public Network Node having
+ * connectivity requirements from other Nodes, by a Floating IP Node.
  */
-@Log
+@Slf4j
 @Component(value = FipTopologyModifier.YORC_OPENSTACK_FIP_MODIFIER_TAG)
 public class FipTopologyModifier extends TopologyModifierSupport {
 
     public static final String YORC_OPENSTACK_FIP_MODIFIER_TAG = "yorc-openstack-fip-modifier";
 
+    @Inject
+    private IToscaTypeSearchService toscaTypeSearchService;
+
     @Override
     @ToscaContextual
     public void process(Topology topology, FlowExecutionContext context) {
-        log.info("Processing topology " + topology.getId());
+        log.debug("Processing topology " + topology.getId());
 
         try {
             WorkflowValidator.disableValidationThreadLocal.set(true);
@@ -59,39 +62,53 @@ public class FipTopologyModifier extends TopologyModifierSupport {
     }
 
     private void doProcess(Topology topology, FlowExecutionContext context) {
+
         Csar csar = new Csar(topology.getArchiveName(), topology.getArchiveVersion());
 
-        // for each Service create a node of type ServiceResource
         Set<NodeTemplate> publicNetworksNodes = TopologyNavigationUtil.getNodesOfType(topology, "yorc.nodes.openstack.PublicNetwork", false);
 
-        publicNetworksNodes.forEach(newtworkNodeTemplate -> {
-            final AbstractPropertyValue networkName = newtworkNodeTemplate.getProperties().get("floating_network_name");
+        String fipConnectivityCap = "yorc.capabilities.openstack.FIPConnectivity";
+        String fipNodeType = "yorc.nodes.openstack.FloatingIP";
+        NodeType fipType = toscaTypeSearchService.findMostRecent(NodeType.class, fipNodeType);
+
+        publicNetworksNodes.forEach(networkNodeTemplate -> {
 
             for (NodeTemplate nodeTemplate : new ArrayList<>(topology.getNodeTemplates().values())) {
-                if (nodeTemplate.getRelationships() == null) continue;
-                nodeTemplate.getRelationships().forEach((rel, relationshipTemplate) -> {
-                    if (relationshipTemplate.getTarget().equals(newtworkNodeTemplate.getName())) {
 
-                        Map<String, AbstractPropertyValue> properties = new LinkedHashMap<>();
-                        properties.put("floating_network_name", networkName);
+                if (nodeTemplate.getRelationships() == null) continue;
+
+                nodeTemplate.getRelationships().forEach((rel, relationshipTemplate) -> {
+
+                    if (relationshipTemplate.getTarget().equals(networkNodeTemplate.getName())) {
 
                         Map<String, Capability> capabilities = new LinkedHashMap<>();
                         Capability connectionCap = new Capability();
-                        connectionCap.setType("yorc.capabilities.openstack.FIPConnectivity");
+                        connectionCap.setType(fipConnectivityCap);
                         capabilities.put("connection", connectionCap);
 
-                        String fipName = "FIP" + nodeTemplate.getName();
+                        if (fipType == null) {
+                            context.log().error("Node type with name <{}> cannot be found in the catalog.",
+                            fipNodeType);
+                            return;
+                        }
 
-                        NodeTemplate nt = addNodeTemplate(csar, topology, fipName, "yorc.nodes.openstack.FloatingIP", "1.0.0");
-                        nt.setProperties(properties);
+                        NodeTemplate nt = replaceNode(
+                            csar,
+                            topology,
+                            networkNodeTemplate,
+                            fipType.getElementId(),
+                            fipType.getArchiveVersion());
+
                         nt.setCapabilities(capabilities);
+                        relationshipTemplate.setRequirementType(fipConnectivityCap);
 
-                        relationshipTemplate.setTarget(fipName);
-                        relationshipTemplate.setRequirementType("yorc.capabilities.openstack.FIPConnectivity");
+                        context.log().info(
+                            "Template <{}> will be modified to be a Floating IP provider for <{}>.",
+                            networkNodeTemplate.getName(),
+                            nodeTemplate.getName());
                     }
                 });
             }
         });
-        publicNetworksNodes.forEach(pnn -> removeNode(topology, pnn));
     }
 }
