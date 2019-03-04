99082fb68fa134310465c3ccdfcb5c244e271928
stephane benoist <29753229+stebenoist@users.noreply.github.com>
2019-03-04 17:13:52 +0100

    Adapt samples

--- tosca-samples/org/ystia/yorc/samples/job/slurm/components/mpi/bin/submit.sh
+++ tosca-samples/org/ystia/yorc/samples/job/slurm/components/mpi/bin/submit.sh
@@ -7,8 +7,8 @@
 #SBATCH --nodes=4
 
 echo "First srun"
-srun --mpi=pmi2 test1.mpi ${NAME1}
+srun --mpi=pmi2 bin/test1.mpi ${NAME1}
 echo "Second srun"
-srun --mpi=pmi2 test2.mpi ${NAME2}
+srun --mpi=pmi2 bin/test2.mpi ${NAME2}
 echo "Third srun"
-srun --mpi=pmi2 test3.mpi ${NAME3}
+srun --mpi=pmi2 bin/test3.mpi ${NAME3}
--- tosca-samples/org/ystia/yorc/samples/job/slurm/components/mpi2/bin/submit.sh
+++ tosca-samples/org/ystia/yorc/samples/job/slurm/components/mpi2/bin/submit.sh
@@ -7,8 +7,8 @@
 #SBATCH --nodes=4
 
 echo "First srun"
-srun --mpi=pmi2 test.mpi ${NAME1}
+srun --mpi=pmi2 /home_nfs/commons/test.mpi ${NAME1}
 echo "Second srun"
-srun --mpi=pmi2 test.mpi ${NAME2}
+srun --mpi=pmi2 /home_nfs/commons/test.mpi ${NAME2}
 echo "Third srun"
-srun --mpi=pmi2 test.mpi ${NAME3}
+srun --mpi=pmi2 /home_nfs/commons/test.mpi ${NAME3}
