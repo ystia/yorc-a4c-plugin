ecd59cdbc762a0667ce136b31b81db779d335af9
stephane benoist <29753229+stebenoist@users.noreply.github.com>
2019-03-04 16:36:19 +0100

    Add mpi2 new slurm job sample

---
+++ tosca-samples/org/ystia/yorc/samples/job/slurm/components/mpi2/bin/submit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+#SBATCH --job-name=batch_test
+#SBATCH --output=res_mpi.out
+#
+#SBATCH --ntasks=4
+#SBATCH --nodes=4
+
+echo "First srun"
+srun --mpi=pmi2 test.mpi ${NAME1}
+echo "Second srun"
+srun --mpi=pmi2 test.mpi ${NAME2}
+echo "Third srun"
+srun --mpi=pmi2 test.mpi ${NAME3}
