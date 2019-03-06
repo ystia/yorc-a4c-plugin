43c5d605f489faf2c6c8d0a36535fa1ad0fad800
stephane benoist <29753229+stebenoist@users.noreply.github.com>
2019-03-06 14:19:27 +0100

    Change mnist run command script

--- tosca-samples/org/ystia/yorc/samples/job/slurm/singularity/components/mnist/bin/run_cmd.sh
+++ tosca-samples/org/ystia/yorc/samples/job/slurm/singularity/components/mnist/bin/run_cmd.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 set -o pipefail
 
-script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
 echo "date debut " > /out/output.txt
 echo "`date`" >> /out/output.txt
-python3 ${script_dir}/mnist_deep.py --data_dir /data | tee -a /out/accuracy.out
+python3 bin/mnist_deep.py --data_dir /data | tee -a /out/accuracy.out
 echo "date fin " >> /out/output.txt
 echo "`date`" >> /out/output.txt
