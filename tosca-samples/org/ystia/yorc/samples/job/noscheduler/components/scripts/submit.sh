8b4504e8ba2442067be94cceabf33c70111c84ff
Laurent Ganne <ganne.laurent@gmail.com>
2019-06-05 13:39:34 +0000

    Removed un-needed line, relace backticj notation

--- tosca-samples/org/ystia/yorc/samples/job/noscheduler/components/scripts/submit.sh
+++ tosca-samples/org/ystia/yorc/samples/job/noscheduler/components/scripts/submit.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
 echo "Submitting job..."
-export TOSCA_JOB_ID="Job-${RANDOM}"
 
-JOB_TIMESTAMP=`/bin/date +%s%N`
+JOB_TIMESTAMP=$(/bin/date +%s%N)
 
 nohup bash -c "$COMMAND_TO_SPAWN"  < /dev/null 2> /tmp/stderr_job_$JOB_TIMESTAMP > /tmp/stdout_job_$JOB_TIMESTAMP &
 JOB_PID=$!
